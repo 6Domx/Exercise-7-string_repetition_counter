f5640656aab4ee65aed28d43a5edfbec5b6ea45b
unknown <miggy.payumo26@gmail.com>
2024-01-26 22:12:48 +0800

    Pseudocode

---
+++ Exercise-7-string_repetition_counter.py
@@ -0,0 +1,5 @@
+# Pseudocode
+# 1. Think of a string that we want to set as an example
+# 2. Using the string example, identify how many times it will appear in an example sentence
+# 3. Return the amount of times it was used
+
