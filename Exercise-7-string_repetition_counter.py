76945f116a023da28bda39bd08dbd762ff782f4b
unknown <miggy.payumo26@gmail.com>
2024-01-26 22:15:07 +0800

    Example sentence set

--- Exercise-7-string_repetition_counter.py
+++ Exercise-7-string_repetition_counter.py
@@ -3,3 +3,4 @@
 # 2. Using the string example, identify how many times it will appear in an example sentence
 # 3. Return the amount of times it was used
 
+sentence_example = "I am Miguel Dominic Payumo, from BSCpE. Miguel is pretty boring so most of my friends call me Migo."
