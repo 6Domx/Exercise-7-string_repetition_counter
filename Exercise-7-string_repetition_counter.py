f85b152c506268979c90f1d728cf95e8708a2c19
unknown <miggy.payumo26@gmail.com>
2024-01-26 22:24:10 +0800

    Code fixed, it will now ask what word would you like to look for.

--- Exercise-7-string_repetition_counter.py
+++ Exercise-7-string_repetition_counter.py
@@ -3,8 +3,15 @@
 # 2. Using the string example, identify how many times it will appear in an example sentence
 # 3. Return the amount of times it was used
 
+# Input for the sentence to be analyzed.
 sentence_example = input("Please input something: ")
+string_sample = input("What word would you like to look for? ")
 
-string_sample_count = sentence_example.count("Miguel")
+# Command for the program to count how many "Miguel" was inputted by the user.
+string_sample_count = sentence_example.count(string_sample)
+
+# Printing of chosen word.
 print("Your chosen word is: Miguel")
+
+# Printing of how many times it appered.
 print("It appered ", string_sample_count, " times.")
